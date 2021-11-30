b90a51a40d8de089ca89cdb31a3a25e39474f98c
S2B2 <boud0164@algonquinlive.com>
2021-11-30 01:26:42 -0500

    Commit 5: Image

--- app/src/main/java/algonquin/cst2335/boud0164/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/boud0164/MainActivity.java
@@ -2,14 +2,18 @@ package algonquin.cst2335.boud0164;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.security.KeyStore;
 
@@ -22,29 +26,62 @@ public class MainActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.textview);
         Button mybutton = findViewById(R.id.mybutton);
         EditText myedit = findViewById(R.id.myedittext);
-        String editString = myedit.getText().toString();
-        myedit.setText("Your edit text has: " + editString);
+        CheckBox checkBox = findViewById(R.id.check);
+        RadioButton radioButton = findViewById(R.id.radio);
+        Switch sw = findViewById(R.id.switch1);
+        ImageView myimage = findViewById( R.id.myimage);
+        ImageButton ib = findViewById(R.id.button3);
+
         mybutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                myedit.setText("Your edit text has: " + editString);
+                String editString = myedit.getText().toString();
+                mybutton.setOnClickListener(vw -> {
+                    mytext.setText("Your edit text has: " + myedit.getText());
+                });
             }
         });
+        sw.setOnCheckedChangeListener((b, c) -> {
+            Context context = getApplicationContext();
+            CharSequence text = "You clicked on the Switch and it is now:" + c;
+            int duration = Toast.LENGTH_SHORT;
 
-        CheckBox checkBox = findViewById(R.id.check);
-
-
-        RadioButton radioButton = findViewById(R.id.radio);
-        Switch switch1 = findViewById(R.id.switch1);
-        switch1.setOnCheckedChangeListener(( button, onOrOff) -> {
-            radioButton.setChecked(onOrOff);
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
         });
 
-        checkBox.setOnCheckedChangeListener(( b, c) -> {
-           if (c)
-               radioButton.setChecked(true);
-           else
-               radioButton.setChecked(false);
+        checkBox.setOnCheckedChangeListener((b, c) -> {
+            Context context = getApplicationContext();
+            CharSequence text = "You clicked on the Checkbox and it is now:" + c;
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+
+        });
+
+        radioButton.setOnCheckedChangeListener((b, c) -> {
+            Context context = getApplicationContext();
+            CharSequence text = "You clicked on the Radiobutton and it is now:" + c;
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+
+
+        });
+        ib.setOnClickListener( (vw) -> {
+            int width = vw.getWidth();
+
+            int height = vw.getHeight();
+
+            Context context = getApplicationContext();
+            CharSequence text = "The width = " + width + " and height = " + height ;
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+
         });
 
 
